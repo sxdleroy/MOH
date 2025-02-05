9bdedde56f0340de0019f774e29372aadd540228
sxdleroy <leroy.hing@gmail.com>
2025-02-05 12:43:13 +1200

    Added comments

--- Programs/movefile.py
+++ Programs/movefile.py
@@ -1,8 +1,12 @@
+# Move a file from one folder to another folder
 import shutil
 
+# Source file path & Destination folder path
 source_file = "/home/leroy/Desktop/Test/Payslip.pdf"
 destination_folder = "/home/leroy/Desktop/Test/Backup"
 
+# Move file
 shutil.move(source_file, destination_folder)
 
-print("File moved successfully"
+# Print message
+print("File moved successfully")
